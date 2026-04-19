71b11134de4e22652fdd6d88ef55bd7a4277d274
Gang Ferdinand Dinga <dingagang@yahoo.com>
2026-04-19 13:35:48 -0500

    Implement IDocumentSession from marten library and update the global Using file to keep code cleaner

--- src/Services/Catalog/CatalogAPI/Feuture/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Feuture/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,9 @@
-﻿using BuildingBlocks.CQRS;
-using CatalogAPI.Model;
-
-namespace CatalogAPI.Feuture.CreateProduct
+﻿namespace CatalogAPI.Feuture.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string>Category, string Description, string ImageFile, decimal Price)
         :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductHandler (IDocumentSession session): ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -20,10 +17,12 @@ namespace CatalogAPI.Feuture.CreateProduct
                Price = command.Price
             };
 
-            //save to database
+            //save to database // this is using the Marten library which transforms the PostgreSQL database to document database
+            session.Store(product);
+            session.SaveChangesAsync(cancellationToken);
 
             //return created results
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
