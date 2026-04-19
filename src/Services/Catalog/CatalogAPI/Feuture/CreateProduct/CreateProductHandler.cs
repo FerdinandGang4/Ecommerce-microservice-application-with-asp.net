0157c5fa11f99d612611dd558d4297cea6bf3957
Gang Ferdinand Dinga <dingagang@yahoo.com>
2026-04-19 10:25:46 -0500

    CreateProductHandler implementation

--- src/Services/Catalog/CatalogAPI/Feuture/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Feuture/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,30 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Model;
+using MediatR;
 
 namespace CatalogAPI.Feuture.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string>Category, string Description, string ImageFile, decimal Price)
-        :IRequest<CreateProductResult>;
+        :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //create product entity
+            var product = new Product
+            {
+               Name = command.Name,
+               Category = command.Category,
+               Description = command.Description,
+               ImageFile = command.ImageFile,
+               Price = command.Price
+            };
+
+            //save to database
+
+            //return created results
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
